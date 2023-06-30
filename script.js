c1b72c46d5614136870be03a1b89e91ca125710c
Sayan Ghosh <sayanghosh379@gmail.com>
2023-06-30 13:00:09 +0530

    decleard variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+const maxScore = document.getElementById("max-score");
